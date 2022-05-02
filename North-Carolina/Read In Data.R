1d706a729e1c70bfcbdc8a7999e818c56bc4aa7c
earcher4 <104062305+earcher4@users.noreply.github.com>
2022-05-02 10:11:09 +0100

    Update and rename data.R to Read In Data.R

--- North-Carolina/Read In Data.R
+++ North-Carolina/Read In Data.R
@@ -4,7 +4,7 @@ spdf <- geojson_read("Voting_Precincts (1).geojson",  what = "sp")
 
 library("readxl")
 library("dplyr")
-D = read_excel("finl.xlsx")
+D = read_excel("NC_data.xlsx")
 D = D[order(D$OBJECTID),]
 
 eta = D$DISTRICT
